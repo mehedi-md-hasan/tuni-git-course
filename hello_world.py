3bfc92764538588cd51dc737e1d29133e61b5c4e
MD MEHEDI HASAN <mmh.hasan04@gmail.com>
2025-07-24 20:35:09 +0300

    Add print statement in release branch

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
+ HEAD
 print("Hello from feature")
 print("Hello from release")
-
-=======
 print("Hello world!")
->>>>>>> upstream/master
+ upstream/master
+print("Hello from release")
